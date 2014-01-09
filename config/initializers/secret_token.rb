a7a4f8d92c9b1678f219e7b678960a32e334394a
YuYang <kristy_yy@126.com>
2014-01-09 10:59:14 +0800

    asdfdsa asdfasf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BbsDemo::Application.config.secret_token = 'c0cfc448119cd11ff5ee7e74c378340aea1b1dd2493eec1adcfecd27e65a03bd4d4d3a5e90725948924a056bab8a8cb4c27ea39510adfe50da1aea5021c6878f'
